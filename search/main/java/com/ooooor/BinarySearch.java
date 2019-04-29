f59143e368b5653612c1361e45a22804097e39b9
rong.chen <18649808198@163.com>
2019-04-29 14:58:18 +0800

    add binary_search

---
+++ search/main/java/com/ooooor/BinarySearch.java
@@ -0,0 +1,51 @@
+package main.java.com.ooooor;
+
+/**
+ * @description:
+ * @author: chenr
+ * @date: 19-2-25
+ */
+public class BinarySearch {
+
+    private static int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    // 二分查找的递归实现
+    private static int bsearchRecursive(int[] a, int n, int val) {
+        return bsearchInternally(a, 0, n - 1, val);
+    }
+
+    private static int bsearchInternally(int[] a, int low, int high, int value) {
+        if (low > high) return -1;
+
+        int mid =  low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bsearchInternally(a, mid+1, high, value);
+        } else {
+            return bsearchInternally(a, low, mid-1, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 6};
+        System.out.println(bsearch(a, a.length, 4));
+    }
+
+}
