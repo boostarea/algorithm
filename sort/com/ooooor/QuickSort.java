e9f44f007f0cb16f2a3a2e8d30b3156d436e4120
rong.chen <18649808198@163.com>
2019-04-29 10:04:57 +0800

    add quick_sort

---
+++ sort/com/ooooor/QuickSort.java
@@ -0,0 +1,55 @@
+package com.ooooor;
+
+import java.util.Arrays;
+
+/**
+ * @Description TODO
+ * @Author ooooor
+ * @Date 2019-04-29 09:49
+ **/
+public class QuickSort {
+
+    // 快速排序，a是数组，n表示数组的大小
+    private static void quickSort(int[] a, int n) {
+        quickSortInternally(a, 0, n-1);
+    }
+
+    // 快速排序递归函数，p,r为下标
+    private static void quickSortInternally(int[] a, int p, int r) {
+        if (p >= r) return;
+
+        int q = partition(a, p, r); // 获取分区点
+        quickSortInternally(a, p, q-1);
+        quickSortInternally(a, q+1, r);
+    }
+
+    private static int partition(int[] a, int p, int r) {
+        int pivot = a[r];
+        int i = p;
+
+        // 保证空间复杂度在O(1)
+        // 极其巧妙，i为要最终与pivot交换的点，j为右边未处理区
+        for(int j = p; j < r; ++j) {
+            if (a[j] < pivot) {
+                if (i == j) {
+                    ++i;
+                } else {
+                    int tmp = a[i];
+                    a[i++] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+        int tmp = a[i];
+        a[i] = a[r];
+        a[r] = tmp;
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 1, 5, 6, 3, 2};
+        quickSort(a, a.length);
+        System.out.println(Arrays.toString(a));
+    }
+}
